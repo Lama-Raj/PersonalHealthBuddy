1f6d1c37a2e74f579141ee4a191c64e451ace739
Raj Lama <razzzzzlama@gmail.com>
2025-10-17 21:12:11 -0400

    Added Color code

--- app/src/main/java/com/unh/personalhealthbuddy/ui/theme/Color.kt
+++ app/src/main/java/com/unh/personalhealthbuddy/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+/* This Color are for my application
+* We are Declaring color names here to use it again and again
+* */
+val PrimaryDarkBlue = Color(0xFF223A6A)
+val ButtonBlue = Color(0xFF4285F4) // A standard blue for buttons
+val White = Color(0xFFFFFFFF)
+val MediumGray = Color(0xFFB0BEC5)    // For borders, dividers, disabled states
+
+val LightBlueBackground = Color(0xFFC8E4EE) // Dashboard Screen Background
+// Dashboard Accent Colors
+val AccentPink = Color(0xFFF5AAB9)
+val AccentOrange = Color(0xFFFABE9B)
+val AccentCyan = Color(0xFF82E2E6)
+val AccentRed = Color(0xFFF19E9E)
+val AccentGreen = Color(0xFFA5D6A7)
