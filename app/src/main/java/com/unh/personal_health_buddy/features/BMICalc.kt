98ae4879535719fcb4b14fcbe687299479c71ae5
Raj Lama <razzzzzlama@gmail.com>
2025-10-29 00:25:51 -0400

    BMI Screen

--- app/src/main/java/com/unh/personal_health_buddy/features/BMICalc.kt
+++ app/src/main/java/com/unh/personal_health_buddy/features/BMICalc.kt
@@ -1,4 +1,23 @@
 package com.unh.personal_health_buddy.features
 
-class BMICalc {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.unh.personal_health_buddy.ui.theme.PersonalHealthBuddyTheme
+
+
+@Composable
+fun BmiScreen(navController: NavController) {
+
+
 }
+
+@Preview(showBackground = true)
+@Composable
+fun BmiScreenPreview() {
+    PersonalHealthBuddyTheme {
+        BmiScreen(navController = rememberNavController())
+    }
+}
+
