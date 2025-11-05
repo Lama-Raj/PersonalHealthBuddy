aae9d74eb2ee1c02a1b10fd8b848b34bddc3fdf7
Raj Lama <razzzzzlama@gmail.com>
2025-11-05 15:16:44 -0500

    Added Color Code that will be used in this page

--- app/src/main/java/com/unh/personal_health_buddy/features/BloodInfo.kt
+++ app/src/main/java/com/unh/personal_health_buddy/features/BloodInfo.kt
@@ -1,6 +1,12 @@
 package com.unh.personal_health_buddy.features
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -10,6 +16,16 @@ import com.unh.personal_health_buddy.ui.theme.PersonalHealthBuddyTheme
 @Composable
 fun BloodGroupScreen(navController: NavController){
 
+    val newGradientStart = Color(0xFFFFFFFF) // White
+    val newGradientEnd = Color(0xFFE3F2FD)   // Shining (Light) Blue
+    val vibrantGradient = Brush.verticalGradient(colors = listOf(newGradientStart, newGradientEnd))
+    val activeColor = Color(0xFF0277BD) // The strong blue from BmiScreen
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(vibrantGradient)
+    )
 
 }
 
