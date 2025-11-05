bdd59f2230565aefcea7ade0e1d4f224fff0e106
Raj Lama <razzzzzlama@gmail.com>
2025-11-05 15:04:22 -0500

    Initial Blood Group Page setup

--- app/src/main/java/com/unh/personal_health_buddy/features/BloodInfo.kt
+++ app/src/main/java/com/unh/personal_health_buddy/features/BloodInfo.kt
@@ -1,4 +1,22 @@
 package com.unh.personal_health_buddy.features
 
-class BloodInfo {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.unh.personal_health_buddy.ui.theme.PersonalHealthBuddyTheme
+
+
+@Composable
+fun BloodGroupScreen(navController: NavController){
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BloodGroupScreenPreview(){
+    PersonalHealthBuddyTheme {
+        BloodGroupScreen(navController = rememberNavController())
+    }
 }
