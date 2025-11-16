cec622dc63c95c4823e21861419d78a8ea06b6e6
Raj Lama <razzzzzlama@gmail.com>
2025-11-15 21:05:03 -0500

    Added Option to Delete prescription

--- app/src/main/java/com/unh/personal_health_buddy/features/medicates.kt
+++ app/src/main/java/com/unh/personal_health_buddy/features/medicates.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Medication
 import androidx.compose.material.icons.filled.Search
@@ -132,8 +133,14 @@ fun MedicateScreen(navController: NavController){
                                 text = "Add Prescription",
                                 onClick = { /* TODO: Handle prescription add */ }
                             )
+                            MiniFabWithText(
+                                icon = Icons.Default.Delete,
+                                text = "Delete Prescription",
+                                onClick = { /* TODO: Handle prescription Delete */ }
+                            )
 
                         }
+
                     }
 
                     // Main FAB
