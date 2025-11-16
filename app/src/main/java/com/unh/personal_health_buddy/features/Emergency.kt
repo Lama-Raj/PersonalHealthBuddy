64ef13fcef03900473ebe38f9a5e922562a1f8b4
Raj Lama <razzzzzlama@gmail.com>
2025-11-15 19:54:04 -0500

    Added Floating button

--- app/src/main/java/com/unh/personal_health_buddy/features/Emergency.kt
+++ app/src/main/java/com/unh/personal_health_buddy/features/Emergency.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -73,6 +75,18 @@ fun EmergencyContactScreen(navController: NavController) {
                         navigationIconContentColor = activeColor
                     )
                 )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /* TODO: Handle add contact */ },
+                    containerColor = activeColor,
+                    contentColor = Color.White
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Contact"
+                    )
+                }
             },
             containerColor = Color.Transparent
         ) { paddingValues ->
